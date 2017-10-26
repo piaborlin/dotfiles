f6f30d8e51edd36d0616ef41239e9580d7aa96a0
Pia Börlin <pia.borlin@gmail.com>
2017-10-26 10:25:07 +0200

    Add config for rvm to bash

--- .bash_profile
+++ .bash_profile
@@ -7,3 +7,5 @@ fi
 # Go
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
--- init.bash
+++ init.bash
@@ -7,6 +7,7 @@ function _link() {
 # Link
 _link dotfiles/.bash_profile ${HOME}/.bash_profile
 _link dotfiles/.bash ${HOME}/.bash
+_link dotfiles/.bashrc ${HOME}/.bashrc
 _link dotfiles/.vimrc ${HOME}/.vimrc
 _link dotfiles/.gitconfig ${HOME}/.gitconfig
 _link dotfiles/.gitignore ${HOME}/.gitignore
