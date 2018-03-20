2bc42e48bf4beccd08b2bf0e1449b2c81fc3b823
Pia Börlin <pia.borlin@gmail.com>
2018-03-20 08:11:55 +0100

    Redis cloud connection helper script
    Takes a Heroku application name and runs redis-cli with password, host,
    password extracted from the REDISCLOUD_URL env variable in the
    application settings.

---
+++ scripts/rediscloud-cli.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Takes a Heroku application name and runs redis-cli with password, host,
+# password extracted from the REDISCLOUD_URL env variable in the application
+# settings.
+
+if [ -z $1 ]; then
+  >&2 echo "Usage: rediscloud-cli <heroku application name>"
+  exit 1
+fi
+
+url=$(heroku config:get REDISCLOUD_URL --app $1)
+
+password=$(sed 's/.*:\(.*\)@.*/\1/' <<< $url)
+host=$(sed 's/.*@\(.*\):.*/\1/' <<< $url)
+port=$(sed 's/.*:\(.*\).*/\1/' <<< $url)
+
+redis-cli -h $host -p $port -a $password
