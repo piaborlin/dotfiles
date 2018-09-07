9280c91702808d71846e01c63f945d23842a38a1
Pia Börlin <pia.borlin@gmail.com>
2018-09-07 14:44:43 +0200

    Add mindnote command to /usr/local/bin

--- init.bash
+++ init.bash
@@ -16,4 +16,5 @@ _link dotfiles/.zshrc ${HOME}/.zshrc
 _link ${HOME}/dotfiles/scripts/travis-script.bash /usr/local/bin/travis-script
 _link ${HOME}/dotfiles/scripts/rediscloud-cli.bash /usr/local/bin/rediscloud-cli
 _link ${HOME}/dotfiles/scripts/dexer.bash /usr/local/bin/dexer
+_link ${HOME}/dotfiles/scripts/mindnote.bash /usr/local/bin/mindnote
 _link ${HOME}/dotfiles/fish ${HOME}/.config
